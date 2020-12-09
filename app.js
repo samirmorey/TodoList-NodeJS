ce0d797c3409116e96face7771d37e51dde34ae8
samirmorey <73993138+samirmorey@users.noreply.github.com>
2020-12-09 10:07:00 +0530

    Correcting port

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const port = process.env.PORT || 3000;
 const cl = console.log
 
 const app = express();
@@ -136,7 +137,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
 
